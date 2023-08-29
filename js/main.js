18e37c1e588ae5a7c38e9679f64d05d780a31c81
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-29 22:34:39 +0500

    Make site-header responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-var splide = new Splide(".splide", {
+// elements
+const elNavOpen = document.querySelector("[data-nav-open]");
+const elNavClose = document.querySelector("[data-nav-close]");
+
+// slider
+let splide = new Splide(".splide", {
   perPage: 3,
   type: "loop",
   focus: "left",
@@ -9,3 +14,14 @@ var splide = new Splide(".splide", {
 });
 
 splide.mount();
+
+// events
+elNavOpen.addEventListener("click", e => {
+  document.querySelector(elNavOpen.dataset.navOpen).classList.add("header__nav--open");
+  document.body.classList.add("body--hidden");
+});
+
+elNavClose.addEventListener("click", () => {
+  document.querySelector(elNavClose.dataset.navClose).classList.remove("header__nav--open");
+  document.body.classList.remove("body--hidden");
+});
