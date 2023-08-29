08ad28b0bfb16161139b906623ba9633defdfe7d
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-29 19:30:01 +0500

    Add hovers, decorations and complete promo section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var splide = new Splide(".splide", {
+  perPage: 3,
+  type: "loop",
+  focus: "left",
+  gap: 1,
+  padding: { left: 10, right: 20 },
+  perMove: 1,
+  autoplay: true,
+});
+
+splide.mount();
